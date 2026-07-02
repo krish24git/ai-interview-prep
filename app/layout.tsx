6a8d5f3098e371738ba25de983a5a94e0eb3b4fa
Krish Kakkar <krishkakkar17@gmail.comgmail.com>
2026-07-02 14:29:24 +0530

    Added interview history and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
-}
+     <body className="min-h-full flex flex-col">
+  <main className="flex-1">
+    {children}
+  </main>
+
+  <Footer />
+</body>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,36 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="bg-slate-900 text-center py-8 text-gray-400">
+    <footer className="bg-slate-900 text-white mt-20 border-t border-slate-700">
+      <div className="max-w-7xl mx-auto px-6 py-6 flex flex-col md:flex-row justify-between items-center gap-4">
+        <div>
+          <h2 className="text-lg font-semibold">Krish Kakkar</h2>
+          <p className="text-gray-400 text-sm">
+            AI Interview Prep Platform
+          </p>
+        </div>
 
-      © 2026 AI Interview Prep
+        <div className="flex gap-6">
+          <Link
+            href="https://github.com/krish24git"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-400 transition"
+          >
+            GitHub
+          </Link>
 
+          <Link
+            href="https://www.linkedin.com/in/krish-kakkar-651788289"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-400 transition"
+          >
+            LinkedIn
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
